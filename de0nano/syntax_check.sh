27392537121655db6571f3adfd10496bae3d19fe
Nick Grabbs <ngrabbs@albinobiscuit.com>
2022-09-05 21:06:09 -0500

    add de0nano -> but gotta figure out how to setup the Makefile for quartus

---
+++ de0nano/syntax_check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+FILES_WITH_ERRORS=""
+# Iterate over each file with a .bdf or .v extension
+for filename in `ls *.bdf *.v *.sv`
+do
+# Perform a syntax check on the specified file
+    quartus_map cpysys_de0nano --analyze_file=$filename
+  # If the exit code is non-zero, the file has a syntax error
+    if [ $? -ne 0 ]
+    then
+       FILES_WITH_ERRORS="$FILES_WITH_ERRORS $filename"
+    fi
+done
+if [ -z "$FILES_WITH_ERRORS" ]
+then
+    echo "All files passed the syntax check"
+exit 0 else
+    echo "There were syntax errors in the following file(s)"
+    echo $FILES_WITH_ERRORS
+    exit 1
+fi
